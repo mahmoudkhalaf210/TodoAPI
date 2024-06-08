5fdf83d4bba4030070ec8e10145f1ad223c038d6
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-08 17:54:20 +0300

    make add todoitems add one todo

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -14,9 +14,16 @@ app.MapGet("/todoitems", async (TodoDB db) =>
     await db.Todos.ToListAsync()
 );
 
-app.MapGet("/todoitem/{id}", async (int id, TodoDB db) =>
+app.MapGet("/todoitems/{id}", async (int id, TodoDB db) =>
 await db.Todos.FindAsync(id)
 );
+
+app.MapPost("/todoitems", async (TodoItem todo, TodoDB db) =>
+{ 
+        db.Todos.Add(todo);
+       await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 
 app.Run();
