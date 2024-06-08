71db59e2b090bf39c7b724c2f5e102928c2b37ba
Mahmoud Khalifa <MKhalifa@healthinsightsgroup.com>
2024-06-08 17:48:25 +0300

    make GetAllTodos

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -10,5 +10,8 @@ builder.Services.AddDbContext<TodoDB>(opt => opt.UseInMemoryDatabase("TodoList")
 var app = builder.Build();
 
 // Configure Pipeline -- UseMethod -- auth 
+app.MapGet("/todoitems", async (TodoDB db) =>
+    await db.Todos.ToListAsync()
+);
 
 app.Run();
